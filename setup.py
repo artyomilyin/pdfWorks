49f8af49f75e78f66677dc9807dda76fdc8bb169
Artyom Ilyin <artyom_ilyin@mail.ru>
2018-06-09 16:36:06 +0300

    distribution includes INPUT and OUTPUT directories

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ packages = ["idna", "PyPDF2", "img2pdf", "os", "shutil", "datetime"]
 options = {
     'build_exe': {
         'packages':packages,
+        'include_files':['INPUT/', 'OUTPUT/'],
     },
 }
 
 setup(
     name = "MergeToPDF",
     options = options,
-    version = "0.1",
+    version = "0.1.1",
     description = 'Эта программа поможет Вам соединить все ваши изображения и pdf файлы в один большой.\n\
                   Просто положите все нужные файлы в папку Input, запустите converter.exe и заберите готовый файл в папке Output.',
     executables = executables
