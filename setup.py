4774755946ef08d7fc5ba557faedced4c697ae1e
Artyom Ilyin <artyom_ilyin@mail.ru>
2018-06-09 16:20:10 +0300

    setup.py added for windows distribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+from cx_Freeze import setup, Executable
+
+base = None
+
+executables = [Executable("converter.py", base=base)]
+
+packages = ["idna", "PyPDF2", "img2pdf", "os", "shutil", "datetime"]
+options = {
+    'build_exe': {
+        'packages':packages,
+    },
+}
+
+setup(
+    name = "MergeToPDF",
+    options = options,
+    version = "0.1",
+    description = 'Эта программа поможет Вам соединить все ваши изображения и pdf файлы в один большой.\n\
+                  Просто положите все нужные файлы в папку Input, запустите converter.exe и заберите готовый файл в папке Output.',
+    executables = executables
+)
