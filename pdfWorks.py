7a4873f722a5df39d34f536b634771bb46a77599
Artyom Ilyin <artyom_ilyin@mail.ru>
2018-06-18 18:09:19 +0300

    kv file added for MainWindow design, Button press event handled

--- pdfWorks.py
+++ pdfWorks.py
@@ -2,10 +2,19 @@ import kivy
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+from converter import Converter
+
 kivy.require('1.10.0')
 
 
 class MainScreen(GridLayout):
+
+    converter = Converter()
+
+    def on_event(self):
+        print("hey")
 
     def update(self, dt):
         pass
@@ -13,6 +22,9 @@ class MainScreen(GridLayout):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         self.cols = 2
+        btn = Button(text="kek")
+        btn.bind(on_press=self.on_event)
+        #self.add_widget(btn)
 
 
 class PdfWorksApp(App):
