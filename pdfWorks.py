7a79dd76c1c2fdc6272f758702d922129f825729
Artyom Ilyin <artyom_ilyin@mail.ru>
2018-06-29 18:04:58 +0300

    convert button is disabled until files are chosen

--- pdfWorks.py
+++ pdfWorks.py
@@ -9,8 +9,17 @@ class PdfWorks(Frame):
     def show_load_dialog(self):
 
         # TODO set datatypes supported (from Converter class)
-        files_list = askopenfilenames()
-        self.converter.set_input_files(files_list)
+        files_list_temp = askopenfilenames()
+        if files_list_temp:
+            self.files_list = files_list_temp
+
+        if self.files_list:
+            self.convertButton['state'] = 'normal'
+            self.converter.set_input_files(self.files_list)
+            self.loadButton['text'] = "Файлов выбрано: " + str(len(self.files_list))
+        else:
+            self.convertButton['state'] = 'disabled'
+            self.loadButton['text'] = "Выбрать файлы"
 
     def show_save_dialog(self):
 
@@ -23,9 +32,10 @@ class PdfWorks(Frame):
 
         Frame.__init__(self, master)
         self.grid()
+        self.files_list = None
         self.loadButton = Button(master, text="Выбрать файлы", command=self.show_load_dialog)
         self.loadButton.grid()
-        self.convertButton = Button(master, text="Конвертировать", command=self.show_save_dialog)
+        self.convertButton = Button(master, text="Конвертировать", command=self.show_save_dialog, state='disabled')
         self.convertButton.grid()
 
         self.converter = Converter()
