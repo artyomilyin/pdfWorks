4b916fa2879adce1fd66b6169937101d20861cda
Artyom Ilyin <artyom_ilyin@mail.ru>
2018-07-02 17:25:09 +0300

    added PickAndSplitFrame to main window

--- pdfWorks.py
+++ pdfWorks.py
@@ -1,15 +1,16 @@
 from tkinter import Tk
 from tkinter.ttk import Notebook
 
-from frames import PickAndConvertFrame
+from frames import PickAndConvertFrame, PickAndSplitFrame
 
 
 if __name__ == "__main__":
 
+    rows = 0
+
     root = Tk()
     root.iconbitmap(r'assets\favicon.ico')
     root.title("PDF Works")
-    rows = 0
     root.columnconfigure(rows, weight=1)
     root.rowconfigure(rows, weight=1)
 
@@ -19,6 +20,10 @@ if __name__ == "__main__":
     pick_and_convert_frame = PickAndConvertFrame()
     notebook.add(pick_and_convert_frame, text='Склеить')
 
+    pick_and_split_frame = PickAndSplitFrame()
+    notebook.add(pick_and_split_frame, text='Разделить')
+
     root.mainloop()
     # TODO: разделение pdf на разные страницы
     # TODO: несколько вкладок (по функциям)
+    # TODO: drag'n'drop feature
