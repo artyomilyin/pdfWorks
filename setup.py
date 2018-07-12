e07c1f2c0dafac34157663141622922adaec8691
Artyom Ilyin <artyom_ilyin@mail.ru>
2018-07-12 15:34:01 +0300

    setup.py changed, now it can build distos

--- setup.py
+++ setup.py
@@ -1,14 +1,47 @@
 from cx_Freeze import setup, Executable
+import os
+import sys
+
+
+os.environ['TCL_LIBRARY'] = 'C:/Users/aailyin/AppData/Local/Programs/Python/Python36-32/tcl/tcl8.6'
+os.environ['TK_LIBRARY'] = 'C:/Users/aailyin/AppData/Local/Programs/Python/Python36-32/tcl/tk8.6'
 
 base = None
 
-executables = [Executable("pdfWorks.py", base=base)]
+if sys.platform == 'win32':
+    base = "Win32GUI"
 
-packages = ["PyPDF2", "img2pdf", "os", "shutil", "datetime"]
+executables = [Executable(
+    "pdfWorks.py",
+    base=base,
+    icon='assets/favicon.ico',
+)]
+
+shortcut = [
+    ("DesktopShortcut",         # Shortcut
+     "DesktopFolder",           # Directory_
+     "pdfWorks",                # Name
+     "TARGETDIR",               # Component_
+     "[TARGETDIR]pdfWorks.exe", # Target
+     None,                      # Arguments
+     None,                      # Description
+     None,                      # Hotkey
+     None,                      # Icon
+     None,                      # IconIndex
+     None,                      # ShowCmd
+     'TARGETDIR'                # WkDir
+     )
+    ]
+
+packages = ["tkinter", "PyPDF2", "img2pdf", "os", "shutil", "datetime", "ntpath"]
+
 options = {
     'build_exe': {
-        'packages':packages,
-        #  'include_files':['INPUT/', 'OUTPUT/'],
+        'packages': packages,
+        'include_files': ["assets/", "DDLs/tcl86t.dll", "DDLs/tk86t.dll"],
+    },
+    'bdist_msi': {
+        'data': {"Shortcut": shortcut},
     },
 }
 
