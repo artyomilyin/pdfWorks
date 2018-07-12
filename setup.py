b566aa7116a8bebc0520e2fc99fe82f474cd9d07
Artyom Ilyin <artyom_ilyin@mail.ru>
2018-07-12 12:16:08 +0300

    setup.py edited for new build

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@ from cx_Freeze import setup, Executable
 
 base = None
 
-executables = [Executable("converter.py", base=base)]
+executables = [Executable("pdfWorks.py", base=base)]
 
-packages = ["idna", "PyPDF2", "img2pdf", "os", "shutil", "datetime"]
+packages = ["PyPDF2", "img2pdf", "os", "shutil", "datetime"]
 options = {
     'build_exe': {
         'packages':packages,
-        'include_files':['INPUT/', 'OUTPUT/'],
+        #  'include_files':['INPUT/', 'OUTPUT/'],
     },
 }
 
 setup(
-    name = "MergeToPDF",
-    options = options,
-    version = "0.1.1",
-    description = 'Эта программа поможет Вам соединить все ваши изображения и pdf файлы в один большой.\n\
-                  Просто положите все нужные файлы в папку Input, запустите converter.exe и заберите готовый файл в папке Output.',
-    executables = executables
+    name="pdfWorks",
+    options=options,
+    version="0.1.2",
+    description='',
+    executables=executables
 )
