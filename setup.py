9971af9a2040890ca2194e556e6a0da0f1295075
Artyom ILYIN <artyom_ilyin@mail.ru>
2019-03-14 23:20:43 +0300

    new build 0.1.6

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     name="pdfWorks",
     author="Artyom ILYIN",
     options=options,
-    version="0.1.5",
+    version="0.1.6",
     description='pdfWorks',
     executables=executables
 )
